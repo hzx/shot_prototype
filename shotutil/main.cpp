8727ba0e9e7f28688c7df5066e42f04c8e6f404b
Dmitry Dementev <dem@oldschooldev.com>
2015-05-12 05:11:25 +0300

    implement shotutil server supervisor and monitor

--- shotutil/main.cpp
+++ shotutil/main.cpp
@@ -1,6 +1,9 @@
 #include <sys/inotify.h>
 #include <unistd.h>
+#include <signal.h>
+#include <wait.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -16,33 +19,31 @@ using std::ostringstream;
 void displayInotifyEvent(struct inotify_event* e);
 
 
-int main(int argc, const char *argv[]) {
-  if (argc < 2) {
-    cout << "need app argument" << endl;
-    return -1;
-  }
+int notifyId = -1;
+int childPid = -1;
+char *childArgv[2] = {"", nullptr};
+char *childEnvp[2] = {"", nullptr}; 
+int numRead;
+size_t const NAME_MAX = 4096;
+size_t const BUF_SIZE = 10 * (sizeof(struct inotify_event) + NAME_MAX + 1);
+char eventsBuf[BUF_SIZE];
+char *cursor;
+struct inotify_event* event;
+int error;
+string filename;
+string absname;
+string currentDir;
 
-  string filename = argv[1];
-  string currentDir = getCurrentDir();
-  ostringstream buf;
-  buf << currentDir << '/' << filename;
-  string absname = buf.str();
 
-  if (!existsFile(absname)) {
-    cout << "not exist app filename: " << filename << endl;
-    return -1;
-  }
+int runChild(char const* app);
 
-  // parse options
-  cout << "argc: " << argc << '\n';
-  for (int i = 0; i < argc; ++i) {
-    cout << argv[i] << endl;
-  }
 
-  cout << "monitor file: " << absname << endl;
+int monitor() {
+  cout << "main app here" << endl;
+  cout << "childPid: " << childPid << endl;
 
   // inotify init
-  int notifyId = inotify_init();
+  notifyId = inotify_init();
   /* int notifyId = inotify_init1(IN_CLOEXEC);// | IN_NONBLOCK); */
   if (notifyId == -1) {
     cout << "inotify_init error" << endl;
@@ -70,13 +71,7 @@ int main(int argc, const char *argv[]) {
   /*     return -1; */
   /*   } */
 
-  int numRead;
-  size_t NAME_MAX = 4096;
-  size_t BUF_SIZE = 10 * (sizeof(struct inotify_event) + NAME_MAX + 1);
-  char eventsBuf[BUF_SIZE];
-  char *cursor;
-  struct inotify_event* event;
-  int error;
+
 
   // epoll event loop
   // TODO: make epoll like in shot
@@ -93,19 +88,22 @@ int main(int argc, const char *argv[]) {
       cout << "read error" << endl;
     }
 
-    cout << "read " << numRead << " bytes from notifyId" << endl;
+    /* cout << "read " << numRead << " bytes from notifyId" << endl; */
 
     // process all of the events in buffer
     for (cursor = eventsBuf; cursor < eventsBuf + numRead; ) {
       event = (struct inotify_event*)(cursor);
       cursor += sizeof(struct inotify_event) + event->len;
-      displayInotifyEvent(event);
+      /* displayInotifyEvent(event); */
       if (event->name == filename) {
         if (event->mask & IN_DELETE) {
-          cout << "kill " << filename << endl;
+          /* cout << "kill " << filename << endl; */
+          kill(childPid,  SIGKILL);
+          waitpid(childPid, 0, 0);
         }
         if (event->mask & IN_MOVED_TO) {
-          cout << "start " << filename << endl;
+          /* cout << "start " << filename << endl; */
+          runChild(absname.c_str());
         }
       }
       /* if (event->mask & IN_MODIFY) { */
@@ -122,8 +120,84 @@ int main(int argc, const char *argv[]) {
   /*     return -1; */
   /*   } */
   /* } */
-  
   return 0;
+}
+
+
+int runChild(char const* app) {
+  childPid = fork();
+  int error;
+  switch (childPid) {
+    case -1:
+      cout << "error run child app: " << app << endl;
+      return -1;
+    case 0:
+      error = execve(app, childArgv, childEnvp);
+      return 0;
+    default:
+      return monitor();
+  }
+  return 0;
+}
+
+
+int main(int argc, const char *argv[]) {
+  if (argc < 2) {
+    cout << "need app argument" << endl;
+    return -1;
+  }
+
+  filename = argv[1];
+  currentDir = getCurrentDir();
+  ostringstream buf;
+  buf << currentDir << '/' << filename;
+  absname = buf.str();
+
+  if (!existsFile(absname)) {
+    cout << "not exist app filename: " << filename << endl;
+    return -1;
+  }
+
+  // parse options
+  cout << "argc: " << argc << '\n';
+  for (int i = 0; i < argc; ++i) {
+    cout << argv[i] << endl;
+  }
+
+  cout << "monitor file: " << absname << endl;
+
+  return runChild(absname.c_str());
+
+  /* int childPid = fork(); */
+  /* int appId = 0; */
+
+  /* switch (childPid) { */
+  /*   case -1: */
+  /*     cout << "fork app error" << endl; */
+  /*     return -1; */
+  /*   case 0: */
+  /*     cout << "child app enter" << endl; */
+  /*     /1* /2* cout << "before system call" << endl; *2/ *1/ */
+  /*     /1* appId = system(absname.c_str()); *1/ */
+  /*     /1* switch (appId) { *1/ */
+  /*     /1*   case -1: *1/ */
+  /*     /1*     cout << "error run app\n"; *1/ */
+  /*     /1*     return -1; *1/ */
+  /*     /1*   case 0: *1/ */
+  /*     /1*     cout << "error run app - no shell available\n"; *1/ */
+  /*     /1*     return -1; *1/ */
+  /*     /1*   default: *1/ */
+  /*     /1*   cout << "after system call, appId: " << appId << endl; *1/ */
+  /*     /1*   cout << "child app exit" << endl; *1/ */
+  /*     /1*   return 0; *1/ */
+  /*     /1* } *1/ */
+  /*     break; */
+  /*   default: */
+
+  /*     break; */
+  /* } // end of switch childPid */
+  
+  /* return 0; */
 }
 
 
